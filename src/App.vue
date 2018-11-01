d6852d150b5e1290ec1b042c1fde3865454e197c
zm <769735716@qq.com>
2018-11-01 09:58:18 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to My todolist"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,84 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <input id="doInput" v-model="newItem" placeholder="do what?" @keydown.enter="addItem">
+    <ul>
+      <li v-for="(item, index) in items" :key="item.id">
+        <input type="checkbox" :checked="item.isFinished" @click="toggle(item)"><span :class="{deleteLine: item.isFinished}">{{ index + 1 }}.{{ item.content }}</span><span class="finish" v-show="item.isFinished">finished</span><span class="delete" @click="delItem(item, index)">delete</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      newItem: "",
+      items: JSON.parse(window.localStorage.getItem("itemList")) || [],
+      nextItemId: 1
+    };
+  },
+  methods: {
+    addItem() {
+      this.items.push({
+        id: this.nextItemId++,
+        content: this.newItem,
+        isFinished: false
+      });
+      this.newItem = "";
+    },
+    toggle(item) {
+      item.isFinished = !item.isFinished;
+    },
+    delItem(item, index) {
+      this.items.splice(index, 1);
+    }
+  },
+  watch: {
+    items: {
+      handler: function() {
+        window.localStorage.setItem("itemList", JSON.stringify(this.items));
+      },
+      deep: true
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  margin: 0 auto;
+  width: 600px;
+}
+#doInput {
+  height: 25px;
+  width: 300px;
 }
 ul {
+  text-align: left;
+  margin-left: 100px;
   list-style-type: none;
-  padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.finish {
+  margin-left: 10px;
+  background-color: red;
 }
-a {
-  color: #42b983;
+.deleteLine {
+  text-decoration: line-through;
+}
+.delete {
+  margin-left: 10px;
+  cursor: pointer;
+  background-color: orange;
+  visibility: hidden;
+}
+li:hover .delete {
+  visibility: visible;
 }
 </style>
